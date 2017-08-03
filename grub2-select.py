cac8377c2780f7035a790693d79e7cb293241611
Jason Stevens <jay@wizardofthenet.com>
2017-08-03 02:15:36 -0700

    add grub2 efi support

--- grub2-select.py
+++ grub2-select.py
@@ -2,7 +2,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2014 Jason Stevens
+# Copyright (c) 2014-2017 Jason Stevens
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,35 +25,44 @@
 
 import os
 import sys
-import StringIO
+if sys.version_info < (3, 0):
+    import StringIO as io
+else:
+    import io
 
 class grub2select():
     menuCount = 1
     menuData = {}
-    grub2cfgData = StringIO.StringIO()
-    grubEnvData = StringIO.StringIO()
-    grub2cfg = "/boot/grub2/grub.cfg"
+    newEntry = ''
+    grub2cfgData = io.StringIO()
+    grubEnvData = io.StringIO()
+    grub2Path = "/boot/grub2/"
+    grub2cfgFile = "grub.cfg"
+    grub2cfgFileEFI = "grub2.cfg"
+    grub2cfg = os.path.join(grub2Path, grub2cfgFile)
+    grub2cfgEFI = os.path.join(grub2Path, grub2cfgFileEFI)
     grubDefault = str(None)
     grubDefaultMatch = None
-    
+
 
     def parseGrubCfg(self):
+        if os.path.exists(self.grub2cfgEFI):
+            self.grub2cfg = self.grub2cfgEFI
         """read in 'grub.cfg' and 'grub2-editenv list' and parse data"""
         if not os.path.exists(self.grub2cfg):
             print("unable to locate %s, is grub2 installed?" % self.grub2cfg)
             sys.exit(0)
 
-
         # load grub.cfg menu data
-        self.grub2cfgData = StringIO.StringIO()
-        f = open("/boot/grub2/grub.cfg", "r")
+        self.grub2cfgData = io.StringIO()
+        f = open(self.grub2cfg, "r")
         self.grub2cfgData.write(f.read())
         f.close()
         del(f)
         self.grub2cfgData.seek(0)
 
         # load grub2 env data
-        self.grubEnvData = StringIO.StringIO()
+        self.grubEnvData = io.StringIO()
         os.system("grub2-editenv list > /tmp/grub2-env.lst")
         f = open("/tmp/grub2-env.lst", "r")
         self.grubEnvData.write(f.read())
@@ -72,7 +81,7 @@ class grub2select():
                 menu["id"] = line.split("'")[3]
                 self.menuData[self.menuCount] = menu
                 self.menuCount +=1
-                
+
         # parse grubEnv for default
         self.grubDefault = str(None)
         self.grubDefaultMatch = None
@@ -81,9 +90,11 @@ class grub2select():
                 self.grubDefault = line.replace("saved_entry=", "").strip()
 
         # try to match default => menu["id"]
-        for item in sorted(self.menuData):
-            if str(self.menuData[item]["id"]).count(self.grubDefault):
-                self.grubDefaultMatch = item
+        #for item in sorted(self.menuData):
+        #    if str(self.menuData[item]["id"]).count(self.grubDefault):
+        #        self.grubDefaultMatch = self.menuData[item]
+        #        break
+
         # try to match default => menu["name"]
         for item in sorted(self.menuData):
             try:
@@ -92,18 +103,23 @@ class grub2select():
             except:
                 matchData = self.grubDefault
             if self.menuData[item]["name"].count(matchData):
-                self.grubDefaultMatch = item
+                self.grubDefaultMatch = self.menuData[item]
+                break
 
     def selectKernel(self):
         for item in sorted(self.menuData):
-            if item is self.grubDefaultMatch:
-                grubMatch = "*"
-            else:
-                grubMatch = " "
+            grubMatch = " "
+            if self.grubDefaultMatch is not None:
+                if self.menuData[item]['name'] is self.grubDefaultMatch['name']:
+                    grubMatch = "*"
             print("{0}:{2}:{1}".format(item, self.menuData[item]["name"], grubMatch))
         print("")
         self.showGrubDefault()
-        selection = raw_input("select number: ")
+        if sys.version_info < (3, 0):
+            selection = raw_input("select number: ")
+        else:
+            selection = input("select number: ")
+            selection = "{0}".format(selection)
         if not selection.isdigit():
             print("Invalid selection")
             sys.exit(0)
@@ -118,16 +134,23 @@ class grub2select():
             sys.exit(0)
         print("valid selection!\n")
 
-        os.system('grub2-set-default "{0}"'.format(self.menuData[selection]["id"]))
-        os.system('grub2-mkconfig -o /boot/grub2/grub.cfg')
+        self.newEntry = self.menuData[selection]
+
+        os.system('grub2-set-default "{0}"'.format(self.newEntry["name"]))
+        #os.system('grub2-set-default "{0}"'.format(self.newEntry["id"]))
+        self.writeGrubCfg()
 
     def showGrubDefault(self):
         if self.grubDefaultMatch is not None:
-            print("Current Default: {0}".format(self.menuData[self.grubDefaultMatch]["name"]))
+            print("Current Default: {0}".format(self.grubDefaultMatch["name"]))
         else:
             print("Current Default: UNKNOWN")
-    
+
+    def writeGrubCfg(self):
+        os.system('grub2-mkconfig -o %s > /dev/null 2>&1' % self.grub2cfg)
+
     def main(self):
+        self.writeGrubCfg()
         self.parseGrubCfg()
         self.selectKernel()
         self.parseGrubCfg()
